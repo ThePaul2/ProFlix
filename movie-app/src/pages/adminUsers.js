5bb15f2861263c67cc4c78c9c0e3dd8f6275e000
dw05164 <dw05164@uga.edu>
2024-02-20 16:50:26 -0500

    added admin stuff

---
+++ movie-app/src/pages/adminUsers.js
@@ -0,0 +1,16 @@
+// Filename - pages/Catalog.js
+
+import React from 'react';
+import Navbar from '../components/NavbarAdmin';
+import SearchBar from '../components/SearchBar'; // Import the SearchBar component
+
+const Users = () => {
+	return (
+		<div>
+			<Navbar />
+			Promotions PAGE
+		</div>
+	);
+};
+
+export default Users;
