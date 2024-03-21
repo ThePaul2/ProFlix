67c745dc15ab99584b5c1c688edb6135ebe5c1a6
dw05164 <dw05164@uga.edu>
2024-03-21 18:45:05 -0400

    password encryption

--- ecinema-springboot/src/main/java/com/ecinema/springboot/ecinema/springboot/EcinemaSpringbootApplication.java
+++ ecinema-springboot/src/main/java/com/ecinema/springboot/ecinema/springboot/EcinemaSpringbootApplication.java
@@ -2,8 +2,13 @@ package com.ecinema.springboot.ecinema.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 
 @SpringBootApplication
+@Configuration
+@EnableEncryptableProperties
 public class EcinemaSpringbootApplication {
 
 	public static void main(String[] args) {
--- ecinema-springboot/src/main/java/com/ecinema/springboot/ecinema/springboot/model/Payment.java
+++ ecinema-springboot/src/main/java/com/ecinema/springboot/ecinema/springboot/model/Payment.java
@@ -13,6 +13,47 @@ public class Payment {
     private String userId;
     private String type;
     private String status;
+    private EncryptedCard card;
+
+    @Data
+    public static class Card {
+        private String type;
+        private String lastFourNumbers;
+        private String firstName;
+        private String lastName;
+        private int expiryMonth;
+        private int expiryYear;
+        private boolean cvvVerified;
+    }
+
+    @Data
+    public static class EncryptedCard {
+        private String encryptedType;
+        private String encryptedLastFourNumbers;
+        private String encryptedFirstName;
+        private String encryptedLastName;
+        private String encryptedExpiryMonth;
+        private String encryptedExpiryYear;
+        private boolean cvvVerified;
+    }
+}
+
+/* 
+package com.ecinema.springboot.ecinema.springboot.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document(collection = "payments")
+public class Payment {
+
+    @Id
+    private String id;
+    private String userId;
+    private String type;
+    private String status;
     private Card card;
 
     @Data
@@ -26,3 +67,4 @@ public class Payment {
         private boolean cvvVerified;
     }
 }
+*/
