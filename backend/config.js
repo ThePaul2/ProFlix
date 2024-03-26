12a15388c4a1a1169c0b7e944dfd08fec355d32b
Joseph Oh <josephoh810@gmail.com>
2024-03-26 16:52:32 -0400

    updated url for db connection

--- backend/config.js
+++ backend/config.js
@@ -1,8 +1,3 @@
 export const PORT = 8080;
 
-<<<<<<< HEAD
 export const mongoDBURL = 'mongodb+srv://dbUser:vG0JGZNvFY47vsqI@atlascluster.w8s7ltx.mongodb.net/CESDatabase?retryWrites=true&w=majority&appName=AtlasCluster'
-=======
-export const mongoDBURL =
-  'mongodb+srv://dbUser:vG0JGZNvFY47vsqI@atlascluster.w8s7ltx.mongodb.net/CESDatabase?retryWrites=true&w=majority&appName=AtlasCluster';
->>>>>>> b4ca4fd0ce0ff2972dd0d9f3bbdf9f4d98e44f90
