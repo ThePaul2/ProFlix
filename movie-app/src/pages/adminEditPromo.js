8b447aebfb9ee1bd16714898d0d2e21d21619ba8
Bret Jones <45405964+Iceman013@users.noreply.github.com>
2024-03-27 10:45:08 -0400

    Edit promos back button

--- movie-app/src/pages/adminEditPromo.js
+++ movie-app/src/pages/adminEditPromo.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 import Navbar from "../components/NavbarAdmin";
 import TextInput from "../components/TextInput";
 
@@ -10,8 +11,15 @@ const editPromo = () => {
 				className="h-screen pt-16 bg-zinc-900"
 			>
 				<div className="bg-zinc-900 text-white font-bold leading-10 py-10 px-60 w-full h-fit">
-					<h1>Edit Promo</h1>
-					<p>Promo Info</p>
+				<div className="flex flex-row w-full">
+						<div>
+							<h1>Edit Promo</h1>
+							<p>Promo Info</p>
+						</div>
+						<div className="ml-auto">
+							<Link to={`/adminPromo`} className="mr-2 px-4 py-2 bg-green-500 rounded-md text-white hover:bg-green-600 transition duration-300 ease-in-out">Quit Without Saving</Link>
+						</div>
+					</div>
 					
 					<br />
 					<TextInput name="Name" placeholder="Free Popcorn" />
