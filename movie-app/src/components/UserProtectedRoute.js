f850146a31a8e00875a9468c7eead4c2a4566f26
RalphLiniado <rjliniado@gmail.com>
2024-04-29 10:50:02 -0400

    UserProtectedRoute file added

---
+++ movie-app/src/components/UserProtectedRoute.js
@@ -0,0 +1,20 @@
+// Importing React library and Navigate component from react-router-dom
+import { Navigate } from 'react-router-dom';
+import React from 'react';
+
+const TheGuardian = ({ children }) => {
+  // Retrieving user role from local storage
+  const userRole = localStorage.getItem('userRole');
+
+  // Checking if user is not an admin
+  if (userRole !== 'user') {
+    // Redirecting to home page if not admin
+    return <Navigate to="/" />;
+  }
+
+  // Rendering children components if user is admin
+  return children;
+};
+
+// Exporting TheGuardian component as default export
+export default TheGuardian;
